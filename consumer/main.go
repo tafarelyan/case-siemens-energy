34c936ff68d4b45434e3ad8b18a531ef91fbe16b
Tafarel Yan <tafarel.yan@gmail.com>
2024-06-09 10:01:20 -0300

    🎉 Project commit

---
+++ consumer/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/segmentio/kafka-go"
+)
+
+func main() {
+	topic := "siemens_energy"
+	brokerAddress := "kafka:9092"
+
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers:   []string{brokerAddress},
+		Topic:     topic,
+		Partition: 0,
+		MaxBytes:  10e6, // 10MB
+	})
+
+	for {
+		m, err := r.ReadMessage(context.Background())
+		if err != nil {
+			break
+		}
+		fmt.Printf("message at offset %d: %s = %s\n", m.Offset, string(m.Key), string(m.Value))
+	}
+
+	if err := r.Close(); err != nil {
+		log.Fatal("failed to close reader:", err)
+	}
+
+}
---
+++ producer/app.py
@@ -0,0 +1,29 @@
+import json
+import time
+import uuid
+from datetime import datetime
+
+from kafka import KafkaProducer
+from lorem_text import lorem
+
+KAFKA_TOPIC = 'siemens_energy'
+KAFKA_BOOTSTRAP_SERVERS = 'kafka:9092'
+
+producer = KafkaProducer(
+    bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+def generate_message() -> dict:
+    return {
+        'id': str(uuid.uuid4()),
+        'message': lorem.sentence(),
+        'timestamp': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+    }
+
+if __name__ == '__main__':
+    while True:
+        message = generate_message()
+        producer.send(KAFKA_TOPIC, value=message)
+        print(f'Messagem enviada do producer: {message}')
+        time.sleep(10)
